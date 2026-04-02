c0d6cd38fe3feb575da696cac07b2ae1d80d3393
Michał Dziedzic <michaldziedzic.work@gmail.com>
2026-04-02 13:11:30 +0200

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,16 @@
+import useGenres from "@/hooks/useGenres";
+import { link } from "fs";
+import React from "react";
+
+const GenreList = () => {
+  const { genres } = useGenres();
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
