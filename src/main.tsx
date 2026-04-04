5bc0a2cdda13e8ed92e020ebc8a9ce035530a5c3
Michał Dziedzic <michaldziedzic.work@gmail.com>
2026-04-04 14:54:58 +0200

    Implement searching

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme.ts";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
